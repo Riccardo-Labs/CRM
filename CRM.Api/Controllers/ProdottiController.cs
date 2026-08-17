ae912fab90099459f08be12c91dcbf09258db9ac
Riccardo-Labs <riccardo_alliegro@libero.it>
2026-08-18 00:15:36 +0200

    CRUD Prodotto

---
+++ CRM.Api/Controllers/ProdottiController.cs
@@ -0,0 +1,101 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CRM.Data.Models;
+
+namespace CRM.Api.Controllers
+{
+    
+[Route("api/[controller]")]
+[ApiController]
+public class ProdottiController : ControllerBase
+{
+    private readonly CrmContext _context;
+
+    public ProdottiController(CrmContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    // GET: api/Prodotti
+    public async Task<ActionResult<IEnumerable<Prodotto>>> GetProdotti()
+    {
+        return await _context.Prodotti.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    // GET: api/Prodotti/5
+    public async Task<ActionResult<Prodotto>> GetProdotto(int id)
+    {
+        var prodotto = await _context.Prodotti.FindAsync(id);
+
+        if (prodotto == null)
+        {
+            return NotFound();
+        }
+
+        return prodotto;
+    }
+
+    [HttpPost]
+    // POST: api/Prodotti
+    public async Task<ActionResult<Prodotto>> PostProdotto(Prodotto prodotto)
+    {
+        _context.Prodotti.Add(prodotto);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetProdotto), new { id = prodotto.IdProdotto }, prodotto);
+    }
+
+    [HttpPut("{id}")]
+    // PUT: api/Prodotti/5
+    public async Task<IActionResult> PutProdotto(int id, Prodotto prodotto)
+    {
+        if (id != prodotto.IdProdotto)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(prodotto).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ProdottoExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    private bool ProdottoExists(int id)
+    {
+        return _context.Prodotti.Any(e => e.IdProdotto == id);
+    }
+    
+    [HttpDelete("{id}")]
+    // DELETE: api/Prodotti/5
+    public async Task<IActionResult> DeleteProdotto(int id)
+    {
+        var prodotto = await _context.Prodotti.FindAsync(id);
+        if (prodotto == null)
+        {
+            return NotFound();
+        }
+
+        _context.Prodotti.Remove(prodotto);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
+}
--- CRM.Api/Program.cs
+++ CRM.Api/Program.cs
@@ -4,21 +4,21 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // SERVICES
-
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<CrmContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CrmConnection")));
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
